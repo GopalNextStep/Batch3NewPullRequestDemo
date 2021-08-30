e996e211f766041c57ea508f3562820f652185e9
Gopal Kisi <50789497+GKisi27@users.noreply.github.com>
2021-08-30 07:02:03 +0545

    Added odd checking also.

--- pull_request_test.py
+++ pull_request_test.py
@@ -1,6 +1,9 @@
-def get_even(lst):
-  final_lst = []
+def get_even_and_odd(lst):
+  final_even_lst = []
+  final_odd_lst = []
   for l in lst:
     if l%2 == 0:
-      final_lst.append(l)
+      final_even_lst.append(l)
+    else:
+      final_odd_lst.append(l)
   return final_lst
